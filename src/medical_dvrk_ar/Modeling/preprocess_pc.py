c9d026747f6e3e29a40fb59d77df77cb0bf43850
DESKTOP-EGKMFCN\14126 <beileiz@andrew.cmu.edu>
2020-09-22 13:54:08 -0400

    :shit: updatae npy file for key points

--- src/medical_dvrk_ar/Modeling/preprocess_pc.py
+++ src/medical_dvrk_ar/Modeling/preprocess_pc.py
@@ -21,18 +21,27 @@ if __name__ == '__main__':
     downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1, max_nn=30))
 
     # get the vector point into the liver instead of point out
-    r_euler = np.asarray(downpcd.normals)
-    r_euler_rev = r_euler.copy()
-    r_euler_rev[:,-1] *= -1
-    r = R.from_euler('xyz', r_euler_rev, degrees=False)
-
+    r_euler_outward = np.asarray(downpcd.normals)
+    r_euler_inward = r_euler_outward.copy()
+    r_euler_outward[:,-1] *= -1
+    r_inward = R.from_euler('xyz', r_euler_inward, degrees=False)
+    r_outward = R.from_euler('xyz', r_euler_outward, degrees=False)
 
     # get the reverse vector point into the liver
-    normal_quat = r.as_quat()
+    inward_normal_quat = r_inward.as_quat()
+    outward_normal_quat = r_outward.as_quat()
     position = np.asarray(downpcd.points)
-    posAndQuat = np.concatenate((position, normal_quat), axis=1)
-    np.save("../../../data/liverGrid_inv_norm.npy", posAndQuat)
-    print(np.load("../../../data/liverGrid_inv_norm.npy", allow_pickle=True).shape)
+    posAndQuat_inward = np.concatenate((position, inward_normal_quat), axis=1)
+    posAndQuat_outward = np.concatenate((position, outward_normal_quat), axis=1)
+    posAndEuler_outward = np.concatenate((position, r_euler_outward), axis=1)
+    posAndEuler_inward = np.concatenate((position, r_euler_inward), axis=1)
+    np.save("../../../data/liverGrid_inward_quat.npy", posAndQuat_inward)
+    np.save("../../../data/liverGrid_outward_quat.npy", posAndQuat_outward)
+    np.save("../../../data/liverGrid_outward_euler.npy", posAndEuler_outward)
+    np.save("../../../data/liverGrid_inward_euler.npy", posAndEuler_inward)
+
+    print(np.load("../../../data/liverGrid_outward_euler.npy", allow_pickle=True).shape)
+    print(np.load("../../../data/liverGrid_outward_quat.npy", allow_pickle=True).shape)
     o3d.visualization.draw_geometries([downpcd])
 
 
