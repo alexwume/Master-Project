8a30a456a6e9b97b8d4f5d30fec719c62cc4c943
DESKTOP-EGKMFCN\14126 <beileiz@andrew.cmu.edu>
2020-09-22 09:55:56 -0400

    :shit: inverse the norm of the keypoint upy, update the code to generate the key points and its normal

--- src/medical_dvrk_ar/Modeling/preprocess_pc.py
+++ src/medical_dvrk_ar/Modeling/preprocess_pc.py
@@ -20,12 +20,19 @@ if __name__ == '__main__':
     print("Recompute the normal of the downsampled point cloud")
     downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1, max_nn=30))
 
-    r = R.from_euler('xyz', np.asarray(downpcd.normals), degrees=False)
+    # get the vector point into the liver instead of point out
+    r_euler = np.asarray(downpcd.normals)
+    r_euler_rev = r_euler.copy()
+    r_euler_rev[:,-1] *= -1
+    r = R.from_euler('xyz', r_euler_rev, degrees=False)
+
+
+    # get the reverse vector point into the liver
     normal_quat = r.as_quat()
     position = np.asarray(downpcd.points)
     posAndQuat = np.concatenate((position, normal_quat), axis=1)
-    np.save("../../../data/sorted_liverGrid_norm.npy", posAndQuat)
-    print(np.load("../../../data/sorted_liverGrid_norm.npy", allow_pickle=True).shape)
+    np.save("../../../data/liverGrid_inv_norm.npy", posAndQuat)
+    print(np.load("../../../data/liverGrid_inv_norm.npy", allow_pickle=True).shape)
     o3d.visualization.draw_geometries([downpcd])
 
 
