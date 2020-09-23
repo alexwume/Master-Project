46e08f83adeedd8f19b4fc4b91f55c3ef238f175
DESKTOP-BMRK4FD\14122 <beileiz@andrew.cmu.edu>
2020-09-23 00:31:25 -0400

    :shit: update data for testing

--- src/medical_dvrk_ar/Modeling/preprocess_pc.py
+++ src/medical_dvrk_ar/Modeling/preprocess_pc.py
@@ -5,45 +5,35 @@ Cora Sept 21, 2020
 """
 import numpy as np
 import open3d as o3d
-
 print(o3d.__version__)
 
 from scipy.spatial.transform import Rotation as R
 if __name__ == '__main__':
-    print("Load a ply point cloud, print it, and render it")
     pcd = o3d.io.read_point_cloud("../../../data/stl2.ply")
-    print(pcd)
     print(np.asarray(pcd.points))
-    o3d.visualization.draw_geometries([pcd])
+    # o3d.visualization.draw_geometries([pcd])
 
-    print("Compute the normal of the point cloud")
     pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1, max_nn=30))
     dense_normals_outward = np.asarray(pcd.normals)
-    dense_normals_outward[:,-1] *= -1
+    dense_normals_inward = dense_normals_outward.copy()
+    dense_normals_inward[:,-1] = 2*np.pi - dense_normals_outward[:,-1]
 
-    print("Downsample the point cloud with a voxel of 0.02")
     downpcd = pcd.voxel_down_sample(voxel_size=0.02)
-
-    print("Recompute the normal of the downsampled point cloud")
     downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1, max_nn=30))
 
-    # get the vector point into the liver instead of point out
-    normals_outward = np.asarray(downpcd.normals)
-
-    normals_inward = normals_outward.copy()
-    normals_outward[:,-1] *= -1
+    normals_sparse_outward = np.asarray(downpcd.normals)
+    normals_sparse_inward = normals_sparse_outward.copy()
+    normals_sparse_inward[:,-1] *= 2*np.pi - normals_sparse_outward[:,-1]
 
     position = np.asarray(downpcd.points)
-    posAndNormals_outward = np.concatenate((position, normals_outward), axis=1)
-    posAndNormals_inward = np.concatenate((position, normals_inward), axis=1)
-    np.save("../../../data/liverGrid_outward_normals.npy", posAndNormals_outward)
-    np.save("../../../data/liverGrid_inward_normals.npy", posAndNormals_inward)
+    sparse_posAndNormals_outward = np.concatenate((position, normals_sparse_outward), axis=1)
+    sparse_posAndNormals_inward = np.concatenate((position, normals_sparse_inward), axis=1)
+    np.save("../../../data/normals_sparse_outward.npy", normals_sparse_outward)
+    np.save("../../../data/normals_sparse_inward.npy", normals_sparse_inward)
 
     dense_posAndNormals_outward = np.concatenate((np.asarray(pcd.points), dense_normals_outward), axis=1)
-    np.save("../../../data/liverGrid_dense_outward_normals.npy", dense_posAndNormals_outward)
+    np.save("../../../data/normals_dense_outward.npy", dense_posAndNormals_outward)
 
-    print(np.load("../../../data/liverGrid_outward_normals.npy", allow_pickle=True).shape)
-    print(np.load("../../../data/liverGrid_outward_normals.npy", allow_pickle=True).shape)
     o3d.visualization.draw_geometries([downpcd])
     
 
