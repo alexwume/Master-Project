9d56b4950ef91af5c1c4fc34c5e6e9ba0fa0a194
anjalipemmaraju <apemmaraju@cmu.edu>
2020-09-29 16:26:09 -0700

    Making changes to add moving liver functionality

--- src/medical_dvrk_ar/dvrkPlanner/liver_scan_NoWristMovement.py
+++ src/medical_dvrk_ar/dvrkPlanner/liver_scan_NoWristMovement.py
@@ -8,18 +8,14 @@ def main():
     robot = psm('PSM1')
     rate = rospy.Rate(100) # 10hz
     robot.home()
-    position_start = robot.get_current_position()
-    safe_pos = PyKDL.Frame(PyKDL.Rotation(PyKDL.Vector(0, 1, 0),
-                           PyKDL.Vector(1, 0, 0),
-                           PyKDL.Vector(0, 0,-1)), 
-                           PyKDL.Vector(0.05,-0.04,-0.13))
-    robot.move(safe_pos)
 
     step_val_x = 0.004
     step_val_y = 0.001
     step_val_z = 0.0001
     dir = 1
     z_dir = 1
+    amp = 0.05
+    freq = 0.5
 
     for i in range(50):
         for j in range(90):
